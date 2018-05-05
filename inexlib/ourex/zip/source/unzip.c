61818350bda75a753871fe2d205a7e0b83f41aee
Guy Barrand <guy.barrand@gmail.com>
2018-05-05 12:15:35 +0200

    update

--- inexlib/ourex/zip/source/unzip.c
+++ inexlib/ourex/zip/source/unzip.c
@@ -605,11 +605,12 @@ local int unzlocal_GetCurrentFileInfoInternal (
 
 
     /* we check the magic */
-    if (err==UNZ_OK)
+    if (err==UNZ_OK) { /*G.Barrand : add {} */
         if (unzlocal_getLong(&s->z_filefunc, s->filestream,&uMagic) != UNZ_OK)
             err=UNZ_ERRNO;
         else if (uMagic!=0x02014b50)
             err=UNZ_BADZIPFILE;
+    }
 
     if (unzlocal_getShort(&s->z_filefunc, s->filestream,&file_info.version) != UNZ_OK)
         err=UNZ_ERRNO;
@@ -685,11 +686,12 @@ local int unzlocal_GetCurrentFileInfoInternal (
         else
             uSizeRead = extraFieldBufferSize;
 
-        if (lSeek!=0)
+        if (lSeek!=0) { /*G.Barrand : add {}*/
             if (ZSEEK(s->z_filefunc, s->filestream,lSeek,ZLIB_FILEFUNC_SEEK_CUR)==0)
                 lSeek=0;
             else
                 err=UNZ_ERRNO;
+        }
         if ((file_info.size_file_extra>0) && (extraFieldBufferSize>0))
             if (ZREAD(s->z_filefunc, s->filestream,extraField,uSizeRead)!=uSizeRead)
                 err=UNZ_ERRNO;
@@ -710,11 +712,12 @@ local int unzlocal_GetCurrentFileInfoInternal (
         else
             uSizeRead = commentBufferSize;
 
-        if (lSeek!=0)
+        if (lSeek!=0) { /*G.Barrand : add {}*/
             if (ZSEEK(s->z_filefunc, s->filestream,lSeek,ZLIB_FILEFUNC_SEEK_CUR)==0)
                 lSeek=0;
             else
                 err=UNZ_ERRNO;
+        }
         if ((file_info.size_file_comment>0) && (commentBufferSize>0))
             if (ZREAD(s->z_filefunc, s->filestream,szComment,uSizeRead)!=uSizeRead)
                 err=UNZ_ERRNO;
@@ -962,11 +965,12 @@ local int unzlocal_CheckCurrentFileCoherencyHeader (
         return UNZ_ERRNO;
 
 
-    if (err==UNZ_OK)
+    if (err==UNZ_OK) { /*G.Barrand : add {} */
         if (unzlocal_getLong(&s->z_filefunc, s->filestream,&uMagic) != UNZ_OK)
             err=UNZ_ERRNO;
         else if (uMagic!=0x04034b50)
             err=UNZ_BADZIPFILE;
+    }
 
     if (unzlocal_getShort(&s->z_filefunc, s->filestream,&uData) != UNZ_OK)
         err=UNZ_ERRNO;
@@ -1116,7 +1120,8 @@ extern int ZEXPORT unzOpenCurrentFile3 (
       pfile_in_zip_read_info->stream.zalloc = (alloc_func)0;
       pfile_in_zip_read_info->stream.zfree = (free_func)0;
       pfile_in_zip_read_info->stream.opaque = (voidpf)0;
-      pfile_in_zip_read_info->stream.next_in = (Bytef)0;
+      /*G.Barrand : pfile_in_zip_read_info->stream.next_in = (Bytef)0;*/
+      pfile_in_zip_read_info->stream.next_in = 0; /*G.Barrand*/
       pfile_in_zip_read_info->stream.avail_in = 0;
 
       err=inflateInit2(&pfile_in_zip_read_info->stream, -MAX_WBITS);
