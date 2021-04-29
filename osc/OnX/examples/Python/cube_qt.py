64932eff02785653fcdc799e4b7bc0ec28e3ca72
Guy Barrand <guy.barrand@gmail.com>
2021-04-29 16:15:36 +0200

    update

--- osc/OnX/examples/Python/cube_qt.py
+++ osc/OnX/examples/Python/cube_qt.py
@@ -16,33 +16,35 @@ args = sys.argv
 #args.append('-verbose')  
 session = Slash.create_session(args)
 
-ui = session.createUI('page_viewer.onx',1,'Qt')
+ui = session.createUI('page_viewer.onx',True,'Qt')
+if ui == None :
+  print('cue_qt.py : UI creation for Qt failed. OnX Qt driver built ?')
+else :  
+  #uimgr = session.uiMan()
+  #ui = uimgr.create('','page_viewer.onx',[],1)
 
-#uimgr = session.uiManager()
-#ui = uimgr.create('','page_viewer.onx',[],1)
+  import CoinPython as Inventor
+  import HEPVis
 
-import CoinPython as Inventor
-import HEPVis
+  soPage = ui.findSoPage('Viewer')
+  
+  soPage.title.setValue('SoCube')
+  
+  soRegion = soPage.currentRegion()
+  soRegion.color.setValue(Inventor.SbColor(0,0,0))
+  soSep = soRegion.cast_SoDisplayRegion().getStaticScene()
+  
+  soCube = Inventor.SoCube()
+  soCube.width.setValue(4)   # Then no more a cube !
 
-soPage = ui.findSoPage('Viewer')
+  soSep.addChild(soCube)
+  
+  del soCube
+  del soSep
+  del soRegion
+  del soPage
 
-soPage.title.setValue('SoCube')
+  ui.steer()
 
-soRegion = soPage.currentRegion()
-soRegion.color.setValue(Inventor.SbColor(0,0,0))
-soSep = soRegion.cast_SoDisplayRegion().getStaticScene()
-
-soCube = Inventor.SoCube()
-soCube.width.setValue(4)   # Then no more a cube !
-
-soSep.addChild(soCube)
-
-del soCube
-del soSep
-del soRegion
-del soPage
-
-ui.steer()
-
-del session
+  del session
 
